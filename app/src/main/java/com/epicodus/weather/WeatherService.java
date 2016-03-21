ac5b0e4315e001bc4b5c70148a101cb68576ed9f
Illia Kuchko and Nathan Romike <student@epicodus.com>
2016-03-21 08:34:00 -0700

    add initials files

---
+++ app/src/main/java/com/epicodus/weather/WeatherService.java
@@ -0,0 +1,17 @@
+package com.epicodus.weather;
+
+import android.content.Context;
+
+/**
+ * Created by Guest on 3/21/16.
+ */
+public class WeatherService {
+    private Context mContext;
+
+    public WeatherService (Context context) {
+        this.mContext = context;
+    }
+
+    
+
+}
